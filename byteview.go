1227f64b9539e4363974216d765ba923abc1ef22
caiyaya <21210860008@m.fudan.edu.cn>
2023-01-28 21:45:43 +0800

    mutex

---
+++ byteview.go
@@ -0,0 +1,27 @@
+package CaiCache
+
+// A ByteView holds an immutable view of bytes.
+type ByteView struct {
+	b []byte // byte类型可以支持任意的数据类型的存储，eg. 字符串、图片等
+}
+
+// Len returns the view's length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// ByteSlice returns a copy of the data as a byte slice.
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string, making a copy if necessary.
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
