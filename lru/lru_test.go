5aef5289270e2103b7ea77c812c239997b60bee7
caiyaya <21210860008@m.fudan.edu.cn>
2023-01-28 21:28:16 +0800

    lRU_test

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,9 @@
 package lru
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type String string
 
@@ -8,6 +11,7 @@ func (d String) Len() int {
 	return len(d)
 }
 
+// 测试get 方法
 func TestGet(t *testing.T) {
 	lru := New(int64(0), nil)
 	lru.Add("key1", String("1234"))
@@ -16,5 +20,39 @@ func TestGet(t *testing.T) {
 	}
 	if _, ok := lru.Get("key2"); ok {
 		t.Fatalf("cache miss key2 failed")
+	}
+}
+
+// 测试，当使用内存超过了设定值时，是否会触发“无用”节点的移除
+func TestRemoveoldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "k3"
+	v1, v2, v3 := "value1", "value2", "v3"
+	cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
+}
+
+// 测试回调函数能否被调用：
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+
+	expect := []string{"key1", "k2"}
+
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
 	}
 }
